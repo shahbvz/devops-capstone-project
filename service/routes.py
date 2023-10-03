0484c9716f03b1bf1d8824518fca45e4f1d5eaec
shahbvz <48415259+shahbvz@users.noreply.github.com>
2023-10-03 16:39:13 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -64,6 +64,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -100,7 +101,9 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
